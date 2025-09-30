b5bfae4b5f59185e985e840b95858fcb3b91373c
Mer-xie <merxie1125@yahoo.>
2025-09-30 21:04:24 +0100

    passed the useCart context into the function

--- cart/src/components/cart/cartFunctionButton.tsx
+++ cart/src/components/cart/cartFunctionButton.tsx
@@ -1,8 +1,24 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import {useCart} from "../../context/cartContext";
+import {Product} from "../../context/cartContext";
 
-export default function AddDelete() {
-    const {count, addCount, removeCount} =useCart();
+interface AddDeleteProps {
+    product: Product[];
+}
+const AddDelete: React.FunctionComponent<AddDeleteProps>=({product}) => {
+    const { cartItems, addToCart, removeFromCart, addCount, removeCount } = useCart();
+    const quantity = cartItems.filter(item => item.name === product.name).length;
+    const handleIncrement = () => {
+        addToCart(product);
+        addCount();
+    };
+    const handleDecrement = () => {
+        const index = cartItems.findIndex(item => item.name === product.name);
+        if (index !== -1) {
+            removeFromCart(index);
+            removeCount();
+        }
+    };
 
         return (
                 <div className='flex justify text-sm w-fit bg-(--orange) text-(--rose50) shadow
@@ -10,18 +26,19 @@ export default function AddDelete() {
                   bottom-5 left-1/2
             -translate-x-1/2 cursor-pointer'>
                         <button
-                            onClick={removeCount}
+                            onClick={handleDecrement}
                             className='cursor-pointer border rounded-full p-[5px] focus:bg-(--rose50)
                              focus:text-(--orange) '>
-                            <img src={'../assets/images/icon-decrement-quantity.svg'}/>
+                            <img src={'../assets/images/icon-decrement-quantity.svg'} alt='increment icon'/>
                         </button>
-                        <span className='px-6 text-sm'>{count}</span>
+                        <span className='px-6 text-sm font-[600]'>{quantity}</span>
                         <button
-                            onClick={addCount}
+                            onClick={handleIncrement}
                             className='cursor-pointer border rounded-full p-[5px] focus:bg-(--rose50) focus:text-(--orange)'>
-                            <img src={"../assets/images/icon-increment-quantity.svg"}/>
+                            <img src={"../assets/images/icon-increment-quantity.svg"} alt='decrement icon'/>
                         </button>
                     </div>
 
         );
     }
+export default AddDelete;
