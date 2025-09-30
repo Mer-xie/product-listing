f0ec827c90eeaf60ffd4f42fe76ce099d528c1a8
Mer-xie <merxie1125@yahoo.>
2025-09-30 20:58:27 +0100

    introduced the usecart context

--- cart/src/components/cart/addTocartButton.tsx
+++ cart/src/components/cart/addTocartButton.tsx
@@ -1,10 +1,19 @@
 import React, {useState} from 'react';
 import AddDelete from "./cartFunctionButton";
+import {useCart} from "../../context/cartContext";
+import {Product} from "../../context/cartContext";
 
-export default function Cart(){
+
+interface CartProps {
+    products: Product[];
+}
+const Cart:React.FC<CartProps> = ({product})=> {
+    const {addToCart, addCount} = useCart()
     const [buttonState, setButtonState] = useState(false)
 
     function handleButtonState(){
+        addToCart(product);
+        addCount();
         setButtonState(true)
     }
 
@@ -16,15 +25,15 @@ export default function Cart(){
 return (
     <div onMouseLeave={toggle}>
         {
-            buttonState ? <AddDelete/> 
+            buttonState ? <AddDelete product={product}/>
                 :
-                <div onMouseDown={handleButtonState}>
-                <button className='bg-(--rose50) shadow border
+                <div>
+                <button onClick={handleButtonState} className='bg-(--rose50) shadow border
                  border-(--orange) py-2 px-6  rounded-full flex cursor-pointer relative
                   bottom-5 left-1/2
             -translate-x-1/2'>
                     <img src={'../assets/images/icon-add-to-cart.svg'}/>
-                    <p className='px-2 text-sm text-(--primary)'>Add to cart</p>
+                    <p className='px-2 text-sm text-(--primary) font-red font-[400]'>Add to cart</p>
                 </button>
                 </div>
         }
@@ -32,4 +41,5 @@ return (
 )
 }
 
+export default Cart;
 
