2c67db8ade57b93ae257f07b56c5921c585ca890
Mer-xie <merxie1125@yahoo.>
2025-09-30 21:05:31 +0100

    manually passed the number into the empty cart logic

--- cart/src/components/cart/cartUi.tsx
+++ cart/src/components/cart/cartUi.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import {useCart} from "../../context/cartContext";
 
 export function EmptyCart(){
-    const {count} = useCart();
-
     return(
         <div className='bg-(--rose50) shadow p-6 h-fit -mt-8 rounded-lg'>
-            <h2 className='text-(--orange) text-2xl font-700 pb-15'>Your Cart ({count})</h2>
+            <h2 className='text-(--orange) text-2xl font-700 pb-15 font-[700]'>Your Cart (0)</h2>
 
             <div className='place-items-center'>
-                <img src={"../assets/images/illustration-empty-cart.svg"} />
-                <p className='text-sm text-(--rose500) opacity-75'>Your added items will appear here</p>
+                <img src={"../assets/images/illustration-empty-cart.svg"} alt='cart-illustration' />
+                <p className='text-sm text-(--rose500) opacity-75 font-[400]'>Your added items will appear here</p>
             </div>
 
         </div>
