cf931b61e4b03c603227fec5d2be15a2bb1748c0
Mer-xie <merxie1125@yahoo.>
2025-09-30 21:07:54 +0100

    passed the logic for the cart to show different components when cart is empty or full

--- cart/src/components/products.tsx
+++ cart/src/components/products.tsx
@@ -3,25 +3,28 @@ import { productsList } from '../data';
 import {EmptyCart} from "./cart/cartUi";
 import {FullCart} from "./cart/fullCart";
 import Cart from "./cart/addTocartButton";
+import {useCart} from "../context/cartContext";
 
 export default function Products() {
+    const {cartItems} = useCart();
+
     return (
         <>
-            <h1 className='font-red text-(--primary) font-700 text-4xl mb-6'>Desserts</h1>
+            <h1 className='font-red text-(--primary) font-700 text-4xl mb-6 font-[700]'>Desserts</h1>
         <div className='grid grid-cols-6 gap-8'>
             <div className='grid xl:grid-cols-3 md:grid-cols-2 sm:grid-rows-1 col-span-4 gap-6'>
             {productsList.map((product, index) => (
                 <div className='' key={index}>
                     <img src={product.image.desktop} alt={product.name} className=' rounded-lg'/>
-                    <Cart/>
-                    <p className='text-(--rose300) text-sm'>{product.category}</p>
-                    <h3 className='text-(--primary) py-1'>{product.name}</h3>
-                    <p className='text-(--orange)'>${product.price}</p>
+                    <Cart product={product}/>
+                    <p className='text-(--rose300) text-sm font-[400]'>{product.category}</p>
+                    <h3 className='text-(--primary) py-1 text-[16px] font-[600]'>{product.name}</h3>
+                    <p className='text-(--orange) font-[600]'>${product.price.toFixed(2)}</p>
                 </div>
             ))}
             </div>
                 <div className='grid gris-cols-1 col-span-2'>
-                <FullCart/>
+                    {cartItems.length === 0 ? <EmptyCart/> : <FullCart/> }
             </div>
         </div>
             </>
