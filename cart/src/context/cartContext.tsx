5a00d668b170eac588669a0577f73b4d6e5053f2
Mer-xie <merxie1125@yahoo.>
2025-09-30 21:02:42 +0100

    created types for the conext

--- cart/src/context/cartContext.tsx
+++ cart/src/context/cartContext.tsx
@@ -1,27 +1,43 @@
 import React, {createContext,useContext, useState} from 'react';
 
-const CartContext= createContext();
-
-export const CartProvider = ({children}) => {
-    const [count,setCount] = useState(0)
-    const [cartItems, setCartItems] = useState([]);
-
-    function addToCart(item){
-        setCartItems((prevItems) =>[...prevItems, item]);
+export type Product = {
+    name: string,
+    price: number,
+    category: string,
+    image:{
+        thumbnail: string,
+        mobile: string,
+        tablet: string,
+        desktop: string,
     }
+}
+type CartContextType = {
+    cartItems:Product[],
+    count: number;
+    addToCart: (item: Product) => void;
+    removeFromCart: (index: number) => void;
+    addCount: () => void;
+    removeCount: () => void;
+    removeAllFromCart: (name: string) => void;
+}
 
-    function addCount(){
-        setCount(count + 1);
-    }
 
-    function removeCount(){
-        if(count > 0){
-            setCount(count - 1);
-        }
-    }
+const CartContext=createContext<CartContextType | undefined>(undefined);
+
+export const CartProvider : React.FC<{children: React.ReactNode}> = ({children}) => {
+    const [count,setCount] = useState<number>(0)
+    const [cartItems, setCartItems] = useState<Product[]>([]);
+    const addToCart = (item: Product) => setCartItems(prev => [...prev, item]);
+    const removeFromCart = (index: number) => setCartItems(prev => prev.filter((_, i) => i !== index));
+    const addCount = () => setCount(prev => prev + 1);
+    const removeCount = () => setCount(prev => (prev > 0 ? prev - 1 : 0));
+    const removeAllFromCart = (name: string) => {
+        setCartItems(prev => prev.filter(item => item.name !== name));
+    };
+
 
     return(
-        <CartContext.Provider value={{count,cartItems, addToCart, addCount, removeCount}}>
+        <CartContext.Provider value={{count,cartItems, addToCart, addCount, removeCount,removeFromCart,removeAllFromCart}}>
             {children}
         </CartContext.Provider>
     )
