7921f28489b93c4a16b7576de5b890a8b93fac44
Mer-xie <merxie1125@yahoo.>
2025-10-04 14:27:16 +0100

    mobile view grid layout

--- cart/src/components/products.tsx
+++ cart/src/components/products.tsx
@@ -11,25 +11,25 @@ export default function Products() {
     return (
         <>
             <h1 className='font-red text-(--primary) font-700 text-4xl mb-6 font-[700]'>Desserts</h1>
-        <div className='grid grid-cols-1 sm:grid-cols-6 gap-8'>
-            <div className='grid xl:grid-cols-3 md:grid-cols-2 sm:grid-rows-1 col-span-4 gap-6'>
-            {productsList.map((product, index) => (
-                <div className='' key={index}>
-                    <img src={product.image.desktop} alt={product.name} className=' rounded-lg'/>
-                    <Cart product={product}/>
-                    <p className='text-(--rose300) text-sm font-[400]'>{product.category}</p>
-                    <h3 className='text-(--primary) py-1 text-[16px] font-[600]'>{product.name}</h3>
-                    <p className='text-(--orange) font-[600]'>${product.price.toFixed(2)}</p>
+            <div className="grid grid-cols-1 sm:grid-cols-6 gap-8 px-4 sm:px-0">
+                <div className="sm:col-span-4 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
+                    {productsList.map((product, index) => (
+                        <div key={index} className="w-full">
+                            <img src={product.image.desktop} alt={product.name} className="rounded-lg w-full " />
+                            <Cart product={product} />
+                            <p className="text-(--rose300) text-sm font-[400]">{product.category}</p>
+                            <h3 className="text-(--primary) py-1 text-[16px] font-[600]">{product.name}</h3>
+                            <p className="text-(--orange) font-[600]">${product.price.toFixed(2)}</p>
+                        </div>
+                    ))}
                 </div>
-            ))}
-            </div>
-            <div className="sm:col-span-2 w-full flex justify-center">
-                <div className="w-full max-w-[600px] sm:max-w-none">
-                    {cartItems.length === 0 ? <EmptyCart /> : <FullCart />}
+                
+                <div className="sm:col-span-2 w-full mt-5">
+                    <div className="w-full">
+                        {cartItems.length === 0 ? <EmptyCart /> : <FullCart />}
+                    </div>
                 </div>
             </div>
-
-        </div>
             </>
     );
 }
